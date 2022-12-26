2ae402215dc0b13116d2755e358504284698f805
HossamKhir <h.khair95@outlook.com>
2022-12-26 12:57:57 +0000

    feat: tested inserting teams; success

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,57 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+# constants
+# INSERT_SUCCESS="INSERT 0 1";
+
+# subroutines
+query_team(){
+  SELECT="SELECT team_id
+  FROM teams
+  WHERE name='$1'";
+  echo $($PSQL "$SELECT");
+}
+
+insert_team(){
+  INSERT="INSERT INTO teams (name)
+  VALUES ('$1');";
+  echo $($PSQL "$INSERT");
+}
+
+# read in the csv file
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != "year" ]]
+  then
+    WINNER_ID=$(query_team "$WINNER");
+    if [[ -z $WINNER_ID ]]
+    then
+      INSERT_TEAM_RESULT=$(insert_team "$WINNER");
+      WINNER_ID=$(query_team "$WINNER");
+    fi
+    OPPONENT_ID=$(query_team "$OPPONENT");
+    if [[ -z $OPPONENT_ID ]]
+    then
+      INSERT_TEAM_RESULT=$(insert_team "$OPPONENT");
+      OPPONENT_ID=$(query_team "$OPPONENT");
+    fi
+    INSERT="INSERT INTO games(
+        year,
+        round,
+        winner_id,
+        opponent_id,
+        winner_goals,
+        opponent_goals
+      )
+    VALUES (
+        $YEAR,
+        '$ROUND',
+        $WINNER_ID,
+        $OPPONENT_ID,
+        $WINNER_GOALS,
+        $OPPONENT_GOALS
+      );";
+    INSERT_GAME_RESULT=$($PSQL "$INSERT");
+  fi
+done
